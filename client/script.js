17c9e911517f1c87d1248f9c2ba8afef20d9b94d
Kanakaiah <98485876+kanakayya@users.noreply.github.com>
2023-01-24 15:13:21 -0500

    update

--- client/script.js
+++ client/script.js
@@ -89,8 +89,7 @@ const handleSubmit = async (e) => {
     const response = await fetch('http://localhost:5000', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': Bearer ${process.env.OPENAI_SECRET_KEY}    
+            'Content-Type': 'application/json',   
         },
         body: JSON.stringify({
             prompt: data.get('prompt')
