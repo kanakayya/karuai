9d5fa9a49a06aa8d751f76f222bf7df651d76641
Kanakaiah <98485876+kanakayya@users.noreply.github.com>
2023-01-25 10:37:40 -0500

    commit

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('http://localhost:5000/users', {
        method: 'POST',
        headers: {
         'Content-Type': 'application/json',
@@ -116,5 +116,5 @@ form.addEventListener('submit', handleSubmit)
 form.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
         handleSubmit(e)
-    }
+    };
 })
